e5c60b02828bc205ca0eed1c2499ed9110db4ae9
PraneshMohanraj <Pranesh.Mohanraj@siriuscom.com>
2022-02-22 20:35:39 +0530

    added longest word problem

--- longestwordlength.java
+++ longestwordlength.java
@@ -2,13 +2,19 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
+	
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
 		String arr[]=str.split("\\s+");
-		Arrays.sort(arr);
+		int max=0;
+		String s=arr[0];
 		for(int i=0;i<arr.length;i++){
-		    System.out.print(arr[i]+" ");
+		    int cur=arr[i].length();
+		    if(cur>max){
+		        s=arr[i];
+		        max=cur;
+		    }
 		}
-
+		System.out.println(s+" "+max);
 	}
 }
